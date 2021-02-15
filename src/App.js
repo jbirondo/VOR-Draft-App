ef63ffc655de1d811437ef0864ee3507c4f62ee1
Eugene Birondo <jbirondo@gmail.com>
2021-02-15 13:42:43 -0800

    Working on Bio

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 import Header from './Header'
 import DraftBoard from './DraftBoard'
+import Bio from './Bio'
 
 class App extends Component {
   render() {
     return (
       <div id="main" className="container-fluid clear-top">
+        <Bio pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
         <Header />
         <DraftBoard />
       </div>
